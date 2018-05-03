983708eb81b04a29a45b23e315a24dc46fc58121
Maël Le Garrec <mlegarrec@online.net>
2018-05-03 08:28:22 +0000

    convert image to new build system

---
+++ 4.9.1/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME =			mattermost
+IMAGE_VERSION =		latest
+IMAGE_VERSION_ALIASES =	4.9.1
+IMAGE_TITLE =			mattermost
+IMAGE_DESCRIPTION =		Open source Slack-alternative in Golang and React
+IMAGE_DOC_URL =
+IMAGE_SOURCE_URL =		https://github.com/scaleway-community/scaleway-mattermost
+IMAGE_VENDOR_URL =		http://mattermost.org
